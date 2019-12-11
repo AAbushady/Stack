d28f2c79e2634251b3290175f8f1e4fbcd6fe73c
Alex Abushady <aabushady214@gmail.com>
2019-12-10 19:48:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            var popCounter = 0;
+
+            Console.WriteLine("Welcome to the stack! " +
+                              "\nType in a value and press enter and it will be added to the stack!" +
+                              "\nWhen you are done just type 'quit' and press enter.");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                popCounter++;
+                stack.Push(input);
+            }
+
+            Console.WriteLine("\nHere are all of the values you typed in from Last to First!\n");
+
+            while (popCounter != 0)
+            {
+                Console.WriteLine(stack.Pop());
+                popCounter--;
+            }
+
+            stack.Push("Hello");
+            stack.Push(" ");
+            stack.Push("World");
+
+            stack.Clear();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private readonly ArrayList _stack = new ArrayList();
+
+        // Stores an object on the stack.
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Null cannot be pushed on to the stack.");
+            }
+
+            _stack.Add(obj);
+        }
+
+        // Removes and returns the object from the top of the stack.
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot pop a value from an empty stack.");
+            }
+
+            var pop = _stack[_stack.Count - 1];
+
+            _stack.RemoveAt(_stack.Count - 1);
+
+            return pop;
+        }
+
+        // Empties out the stack.
+        public void Clear()
+        {
+            var cnt = _stack.Count - 1;
+            while (_stack.Count != 0)
+            {
+                _stack.RemoveAt(cnt--);
+            }
+        }
+    }
+}
